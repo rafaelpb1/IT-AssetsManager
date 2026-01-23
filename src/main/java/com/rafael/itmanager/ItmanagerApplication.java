eea803e796770e1eb38a74209f4d77575e523a45
Rafael <rafael.souza05@cs.unipe.edu.br>
2026-01-23 12:38:05 -0300

    Initial commit

---
+++ src/main/java/com/rafael/itmanager/ItmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.rafael.itmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafael/itmanager/ItmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafael.itmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
