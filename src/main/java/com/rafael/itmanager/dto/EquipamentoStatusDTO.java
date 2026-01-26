b960c84f0b45df539e4191ec5512218fe4ce4a8a
Rafael <rafael.souza05@cs.unipe.edu.br>
2026-01-26 12:50:19 -0300

    Implementando DTO para atualização de status

---
+++ src/main/java/com/rafael/itmanager/dto/EquipamentoStatusDTO.java
@@ -0,0 +1,10 @@
+package com.rafael.itmanager.dto;
+
+import com.rafael.itmanager.model.StatusEquipamento;
+import jakarta.validation.constraints.NotNull;
+
+public record EquipamentoStatusDTO(
+        @NotNull(message = "O novo status deve ser informado")
+        StatusEquipamento status
+) {
+}
