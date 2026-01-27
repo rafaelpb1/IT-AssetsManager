1ddcbbef88527223150399cc1dbc034487c39caf
Rafael <rafael.souza05@cs.unipe.edu.br>
2026-01-27 12:46:05 -0300

    Adicionando UserDetails customizado

---
+++ src/main/java/com/rafael/itmanager/security/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.rafael.itmanager.security;
+
+import com.rafael.itmanager.model.Usuario;
+import com.rafael.itmanager.service.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UsuarioService service;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = service.obterEntidadePorLogin(username);
+
+        if (usuario == null) {
+            throw new UsernameNotFoundException("Usuário não encontrado");
+        }
+
+        return User.builder()
+                .username(usuario.getLogin())
+                .password(usuario.getSenha())
+                .roles("USER")
+                .build();
+    }
+}
