072887bdf1357e3fe8d39ae354bc564b890e3820
Rafael <rafael.souza05@cs.unipe.edu.br>
2026-01-28 12:52:40 -0300

    Autorização de roles direto nas rotas

--- src/main/java/com/rafael/itmanager/controller/ColaboradorController.java
+++ src/main/java/com/rafael/itmanager/controller/ColaboradorController.java
@@ -1,14 +1,14 @@
 package com.rafael.itmanager.controller;
 
-import com.rafael.itmanager.dto.ColaboradorRequestDTO;
-import com.rafael.itmanager.dto.ColaboradorResponseDTO;
+import com.rafael.itmanager.dto.ColaboradorDTOs.ColaboradorRequestDTO;
+import com.rafael.itmanager.dto.ColaboradorDTOs.ColaboradorResponseDTO;
 import com.rafael.itmanager.service.ColaboradorService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -20,12 +20,14 @@ public class ColaboradorController {
     private final ColaboradorService service;
 
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<ColaboradorResponseDTO> cadastrarColaborador(@RequestBody @Valid ColaboradorRequestDTO dto) {
          var colaborador = service.salvarColaborador(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(colaborador);
     }
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     public ResponseEntity<List<ColaboradorResponseDTO>> listarColaboradores() {
         return ResponseEntity.ok(service.listarColaboradores());
     }
