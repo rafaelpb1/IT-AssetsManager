63607fbf4d617e9f00a24d66bb464c1479a8e2a0
Rafael <rafael.souza05@cs.unipe.edu.br>
2026-01-29 12:49:39 -0300

    Rotas das requisições e respostas de grupos

---
+++ src/main/java/com/rafael/itmanager/controller/GrupoController.java
@@ -0,0 +1,33 @@
+package com.rafael.itmanager.controller;
+
+import com.rafael.itmanager.model.Grupo;
+import com.rafael.itmanager.repository.GrupoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/grupos")
+@RequiredArgsConstructor
+public class GrupoController {
+
+    private final GrupoRepository repository;
+
+    @PostMapping
+    @Transactional
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Grupo> salvar(@RequestBody Grupo grupo) {
+        repository.save(grupo);
+        return ResponseEntity.ok(grupo);
+    }
+
+    @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<List<Grupo>> listar() {
+        return ResponseEntity.ok(repository.findAll());
+    }
+}
