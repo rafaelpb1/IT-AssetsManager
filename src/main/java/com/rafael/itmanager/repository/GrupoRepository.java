d49f2656eb0f8dddc545afec41fd52ad602076e0
Rafael <rafael.souza05@cs.unipe.edu.br>
2026-01-29 12:30:15 -0300

    GrupoRepository

---
+++ src/main/java/com/rafael/itmanager/repository/GrupoRepository.java
@@ -0,0 +1,10 @@
+package com.rafael.itmanager.repository;
+
+import com.rafael.itmanager.model.Grupo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface GrupoRepository extends JpaRepository<Grupo, String> {
+    Optional<Grupo> findByNome(String nome);
+}
