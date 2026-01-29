f9fcaf926ad84d85b1a1847cd5130b885ac5ea52
Rafael <rafael.souza05@cs.unipe.edu.br>
2026-01-29 07:33:26 -0300

    Adicionando filtro de auth por meio do header

---
+++ src/main/java/com/rafael/itmanager/config/CustomFilter.java
@@ -0,0 +1,42 @@
+package com.rafael.itmanager.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+public class CustomFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest req,
+                                    HttpServletResponse res,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        String secretHeader = req.getHeader("x-secret");
+
+        if (secretHeader != null) {
+            if (secretHeader.equals("s3cret")) {
+                Authentication authentication = new UsernamePasswordAuthenticationToken(
+                        "Segredo", null, List.of(new SimpleGrantedAuthority("USER")));
+
+                SecurityContext securityContext = SecurityContextHolder.getContext();
+                securityContext.setAuthentication(authentication);
+            }
+        }
+
+        filterChain.doFilter(req, res);
+
+
+    }
+}
