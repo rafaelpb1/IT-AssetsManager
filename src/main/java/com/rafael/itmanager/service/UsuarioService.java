d7b6eddd14fc6568b62c0bdef6d6775fea51cd45
Rafael <rafael.souza05@cs.unipe.edu.br>
2026-01-29 07:31:44 -0300

    lógica de negócio de usuário

--- src/main/java/com/rafael/itmanager/service/UsuarioService.java
+++ src/main/java/com/rafael/itmanager/service/UsuarioService.java
@@ -1,13 +1,16 @@
 package com.rafael.itmanager.service;
 
-import com.rafael.itmanager.dto.UsuarioRequestDTO;
-import com.rafael.itmanager.dto.UsuarioResponseDTO;
+import com.rafael.itmanager.dto.UsuarioDTOs.UsuarioRequestDTO;
+import com.rafael.itmanager.dto.UsuarioDTOs.UsuarioResponseDTO;
 import com.rafael.itmanager.mapper.UsuarioMapper;
 import com.rafael.itmanager.model.Usuario;
 import com.rafael.itmanager.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -24,12 +27,11 @@ public class UsuarioService {
         return mapper.toDTO(usuario);
     }
 
-    public UsuarioResponseDTO obterPorLogin(String login) {
-        Usuario usuario = repository.findByLogin(login);
-        return mapper.toDTO(usuario);
+    public Usuario obterPorLogin(String login) {
+        return repository.findByLogin(login);
     }
 
-    public Usuario obterEntidadePorLogin(String login) {
-        return repository.findByLogin(login);
+    public List<Usuario> listarUsuarios() {
+        return this.repository.findAll();
     }
 }
