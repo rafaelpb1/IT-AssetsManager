196c1b52b70237e3c00364af7cbe158e24dad7b5
Rafael <rafael.souza05@cs.unipe.edu.br>
2026-01-29 12:31:35 -0300

    Rotas das requisições e respostas de Usuario

--- src/main/java/com/rafael/itmanager/controller/UsuarioController.java
+++ src/main/java/com/rafael/itmanager/controller/UsuarioController.java
@@ -1,14 +1,17 @@
 package com.rafael.itmanager.controller;
 
-import com.rafael.itmanager.dto.UsuarioRequestDTO;
-import com.rafael.itmanager.dto.UsuarioResponseDTO;
+import com.rafael.itmanager.dto.UsuarioDTOs.UsuarioRequestDTO;
+import com.rafael.itmanager.dto.UsuarioDTOs.UsuarioResponseDTO;
 import com.rafael.itmanager.mapper.UsuarioMapper;
 import com.rafael.itmanager.model.Usuario;
 import com.rafael.itmanager.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -19,8 +22,19 @@ public class UsuarioController {
     private final UsuarioMapper mapper;
 
     @PostMapping
-    public ResponseEntity<UsuarioResponseDTO> salvar(@RequestBody UsuarioRequestDTO dto) {
-        UsuarioResponseDTO salvar = service.salvar(dto);
-        return  ResponseEntity.status(HttpStatus.CREATED).body(salvar);
+    public ResponseEntity<UsuarioResponseDTO> salvar(@RequestBody UsuarioRequestDTO dto ) {
+        UsuarioResponseDTO salvar = service.salvar(dto, dto.permissoes());
+        return ResponseEntity.status(HttpStatus.CREATED).body(salvar);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> listarUsuarios( ) {
+        List<Usuario> usuarios = service.listarUsuarios();
+        return ResponseEntity.status(HttpStatus.OK).body(usuarios);
+    }
+
+    @GetMapping("/logado")
+    public ResponseEntity<String> rotaLogin(Authentication authentication) {
+        return ResponseEntity.ok("Usuario conectado: " + authentication.getName());
     }
 }
